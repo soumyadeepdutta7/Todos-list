3435e6b33a996703ff839238be0001f142084e75
Soumyadeep Dutta <112638681+soumyadeepdutta7@users.noreply.github.com>
2023-07-31 19:02:18 +0530

    Add files via upload
    Final changes

---
+++ src/MyComponents/About.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h2>Welcome to Todo List App!</h2>
+      <p>
+        This app is a simple and user-friendly task management tool designed to help you stay organized and productive. With this app, you can easily keep track of your daily tasks and delete them as completed when done.
+      </p>
+      <h3>Features:</h3>
+      <ul>
+        <li>Add and Edit Tasks: You can quickly add new tasks to your list and assign them as needed.</li>
+        <li>Task Categories: Organize your tasks by categories. Make accurate task list by providing title and description for the task.</li>
+        <li>Mark as Complete: When you finish a task, you can mark it as Delete. The completed tasks will be removed from task list .</li>
+      </ul>
+      <h3>Why Choose  Todo List App?</h3>
+      <p>
+        This app stands out because of its simplicity and ease of use. We believe in providing a clutter-free and intuitive interface that helps you focus on what matters most - getting things done!
+      </p>
+      <h3>How to Use:</h3>
+      <ul>
+        <li>Adding Tasks: To add a new task, click on the "Add Todo" button and enter the task details. Press "Enter" to save the task.</li>
+        <li>Setting Priority and Deadline: You can mention the deadline and priority of the task in the description section.</li>
+        <li>Completing Tasks: When you finish a task, click the delete button to it to mark it as complete.</li>
+      </ul>
+      <h3>Privacy and Security:</h3>
+      <p>
+        We take your privacy and data security seriously. Our app is built with the latest security measures to ensure your information is safe and confidential. We do not collect any personal information from our users.
+      </p>
+      <h3>Feedback and Support:</h3>
+      <p>
+        We value your feedback! If you have any suggestions or encounter any issues while using our app, please don't hesitate to contact our support team at <a href="mailto:support@example.com">support@example.com</a>. Your feedback helps us improve the app and make it even better for you!
+      </p>
+      <p>
+        Thank you for choosing our Todo List App. We hope it simplifies your life and boosts your productivity. Happy task managing!
+      </p>
+    </div>
+  );
+};
